b574bc6f36d30628a84a15390f49876952b82c35
Hakan AKSOY <aksoyhakan1990gmail.com>
2022-12-29 14:10:38 +0300

    Tüm görevler yapılmıştır.

--- ip/index.js
+++ ip/index.js
@@ -1,32 +1,30 @@
 //axios import buraya gelecek
-
+import axios from "axios";
 var benimIP;
-
 
 // ------------ değiştirmeyin --------------
 // licensed to Ergineer 2022
 require("babel-core/register");
 require("babel-polyfill");
-async function ipAdresimiAl(){
-	await axios({
-		method: 'get',
-		url: 'https://apis.ergineer.com/ipadresim',
-	})
-	.then(function (response) {
-		return response.data
-	})
-	.then(function (a) {
-		benimIP=a
-	});
-}				
+async function ipAdresimiAl() {
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      benimIP = a;
+    });
+  return benimIP;
+}
 // ------------ değiştirmeyin --------------
-
 
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
     (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
     https://apis.ergineer.com/ipgeoapi/<ipniz>
-	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
@@ -36,7 +34,9 @@ async function ipAdresimiAl(){
 	iyice anlamanız gerekmektedir.
 	
 */
+
 /*
+
 	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
     DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
 	
@@ -67,6 +67,60 @@ async function ipAdresimiAl(){
 	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
 */
 
-
-
 //kodlar buraya gelecek
+let newIp;
+let ipAddress = ipAdresimiAl().then((response) => {
+  axios
+    .get(`https://apis.ergineer.com/ipgeoapi/${response}`)
+
+    .then((response) => {
+      const cardsDiv = document.querySelector(".cards");
+      cardsDiv.appendChild(domMaker(response.data));
+    });
+
+  function domMaker(newObject) {
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+
+    const photo = document.createElement("img");
+    photo.src = newObject.ülkebayrağı;
+    cardDiv.appendChild(photo);
+
+    const cardInfoDiv = document.createElement("div");
+    cardInfoDiv.classList.add("card-info");
+
+    const h3 = document.createElement("h3");
+    h3.className = "ip";
+    h3.textContent = newObject.sorgu;
+    cardInfoDiv.appendChild(h3);
+
+    const ulke = document.createElement("p");
+    ulke.classList.add("ulke");
+    ulke.textContent = `${newObject.ülke} (${newObject.ülkeKodu})`;
+    cardInfoDiv.appendChild(ulke);
+
+    const enlem = document.createElement("p");
+    enlem.textContent = `Enlem: ${newObject.enlem} Boylam: (${newObject.boylam})`;
+    cardInfoDiv.appendChild(enlem);
+
+    const sehir = document.createElement("p");
+    sehir.textContent = newObject.şehir;
+    cardInfoDiv.appendChild(sehir);
+
+    const saat = document.createElement("p");
+    saat.textContent = newObject.saatdilimi;
+    cardInfoDiv.appendChild(saat);
+
+    const para = document.createElement("p");
+    para.textContent = newObject.parabirimi;
+    cardInfoDiv.appendChild(para);
+
+    const isp = document.createElement("p");
+    isp.textContent = newObject.isp;
+    cardInfoDiv.appendChild(isp);
+
+    cardDiv.appendChild(cardInfoDiv);
+
+    return cardDiv;
+  }
+});
